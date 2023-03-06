5914dacd50a1112bd15f6f7f4423e198567622a2
George Grishchenkov <goscha-grishenkov@yandex.ru>
2023-03-06 13:19:52 +0300

    add command line arguments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,19 @@ import static java.lang.System.exit;
 
 public class Main {
     public static void main(String[] args) {
-        String jdbcURL = "jdbc:h2:D:/gog/projects/java/database/database";
-        String name = "gosha";
-        String pwd = "";
+        String jdbcURL = "jdbc:h2:", name, pwd, file;
+        if (args.length < 4){
+            System.out.println("ожидался полный путь до бд, логин, пароль, имя выходного файла");
+            exit(1);
+        }
+        for(var a: args){
+            System.out.println(a);
+        }
+        jdbcURL += args[0];
+        name = args[1];
+        pwd = args[2];
+        file = args[3];
+        System.out.println();
         Loader l = new Loader(jdbcURL, name, pwd);
         if (!l.connect()){
             System.out.println("не удалось подкючиться к бд");
@@ -22,7 +32,7 @@ public class Main {
             Processor proc = new Processor(table_list, table_cols);
             proc.process();
             try{
-                proc.print("file.txt");
+                proc.print(file);
             }
             catch (IOException | RuntimeException e){
                 e.printStackTrace();
